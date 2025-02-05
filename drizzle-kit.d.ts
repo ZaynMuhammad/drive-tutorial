ff1e380b5d5a9406a9a664e3858489e728451855
Zayn Muhammad <zaynmuhammad95@gmail.com>
2025-02-05 17:21:47 -0500

    Add singlestore db

---
+++ drizzle-kit.d.ts
@@ -0,0 +1,15 @@
+declare module "drizzle-kit" {
+  export type Config = {
+    schema: string;
+    dialect: "postgresql" | "mysql" | "sqlite" | "singlestore";
+    tablesFilter: string[];
+    dbCredentials?: {
+      host: string;
+      port: number;
+      user: string;
+      password: string;
+      database: string;
+      ssl?: object;
+    };
+  };
+} 
