2ada674f36fad81a07baa600e7826262fdd18008
Zayn Muhammad <zaynmuhammad95@gmail.com>
2025-02-11 18:30:51 -0500

    Update env example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import { db } from "~/server/db"
-import { files as filesSchema, folders as foldersSchema } from "~/server/db/schema"
-import DriveContents from "./drive-contents";
 
 export default function GoogleDriveClone() {
   return <div>Hello everyone</div>
