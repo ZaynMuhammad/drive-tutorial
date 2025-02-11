26121ad5fbc076b45759b9e07c7df1c267fb8956
Zayn Muhammad <zaynmuhammad95@gmail.com>
2025-02-10 19:17:37 -0500

    db model working

--- drizzle-kit.d.ts
+++ drizzle-kit.d.ts
@@ -2,7 +2,7 @@ declare module "drizzle-kit" {
   export type Config = {
     schema: string;
     dialect: "postgresql" | "mysql" | "sqlite" | "singlestore";
-    tablesFilter: string[];
+    // tablesFilter: string[];
     dbCredentials?: {
       host: string;
       port: number;
